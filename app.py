4d997324b126d73ac96919c9f6171613d812c0bc
Shikhar-code <73746913+Shikhar-code@users.noreply.github.com>
2026-03-11 01:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
-import streamlit as st
+import gradio as gr
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from PIL import Image
 
-# Load trained model
 model = load_model("retinopathy_model.h5")
 
 classes = ["Mild", "Moderate", "No_DR", "Proliferate_DR", "Severe"]
 
-st.title("Diabetic Retinopathy Detection")
-
-uploaded_file = st.file_uploader("Upload a retinal image", type=["jpg","png"])
-
-if uploaded_file is not None:
-    
-    img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image", use_column_width=True)
-
+def predict(img):
     img = img.resize((224,224))
-    img_array = np.array(img)
+    img = np.array(img)
 
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
 
-    prediction = model.predict(img_array)
+    prediction = model.predict(img)
+    return classes[np.argmax(prediction)]
 
-    predicted_class = classes[np.argmax(prediction)]
+interface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Diabetic Retinopathy Detection",
+    description="Upload a retinal fundus image to predict the DR stage"
+)
 
-    st.subheader("Prediction:")
-    st.write(predicted_class)
+interface.launch()
